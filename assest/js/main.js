07d2ae25e010d6645dc637741988703877847e8f
ZILLEALI <zilleali1245@gmail.com>
2023-02-25 16:54:08 +0500

    Crazy form update for desktop screen

---
+++ assest/js/main.js
@@ -0,0 +1,8 @@
+
+let username = document.getElementById("username");
+let password = document.getElementById("password");
+let submitBtn = document.getElementById("submit");
+
+let usernamevalid = () => {
+    
+}
